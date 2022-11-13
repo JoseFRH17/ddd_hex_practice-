0cd77bb0d1b088ff552206abadfe4bf1ae125a33
José Fco <josefrodriguezh@gmail.com>
2022-11-13 15:20:45 +0000

    :recycle: Setup for controller test

--- src/test/java/com/jose/dddhexpractice/acceptance/ChampionControllerAcceptanceTest.java
+++ src/test/java/com/jose/dddhexpractice/acceptance/ChampionControllerAcceptanceTest.java
@@ -1,32 +1,30 @@
 package com.jose.dddhexpractice.acceptance;
 
 
-import com.jose.dddhexpractice.controllers.ChampionController;
+import com.jose.dddhexpractice.common.AbstractControllerTest;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 
-@WebMvcTest(ChampionController.class)
-public class ChampionControllerAcceptanceTest {
+public class ChampionControllerAcceptanceTest  extends AbstractControllerTest {
 
-    private final MockMvc mockMvc;
-
-    public ChampionControllerAcceptanceTest(MockMvc mockMvc) {
-        this.mockMvc = mockMvc;
+    @Override
+    @BeforeAll
+    public void setUp() {
+        super.setUp();
     }
 
     @Test
     @DisplayName("Get all champions on empty database should give empty array")
     public void getAllChampionsWithEmptyDatabase() throws Exception {
 
-        MvcResult mvcResult = mockMvc.perform(get("champion/all"))
+        MvcResult mvcResult = mockMvc.perform(get("/champion/all"))
                 .andReturn();
         int status = mvcResult.getResponse().getStatus();
         String data = mvcResult.getResponse().getContentAsString();
